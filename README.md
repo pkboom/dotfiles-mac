5a23db5d4d4b1bb2b47d7dd2481ce10606b4fa8b
Keunbae Park <ppotpo@gmail.com>
2022-06-01 14:47:53 -0400

    wip

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 ### [Setting up your Mac](https://github.com/driesvints/dotfiles#setting-up-your-mac)
 
+1. [Generate a new public and private SSH key](https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent) by running:
+
+   ```zsh
+   curl https://raw.githubusercontent.com/pkboom/dotfiles-mac/master/ssh.sh | sh -s "<your-email-address>"
+   ```
+
+1. Clone this repo to `~/.dotfiles` with:
+
+   ```zsh
+   git clone git@github.com:driesvints/dotfiles.git ~/.dotfiles
+   ```
+
+1. Run the installation with:
+
+   ```zsh
+   ~/.dotfiles/fresh.sh
+   ```
+
+1. After mackup is synced with your cloud storage, restore preferences by running `mackup restore`
+1. Restart your computer to finalize the process
+
 ### [Connecting to GitHub with SSH](https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh)
 
 [Generate a new public and private SSH key](https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent)
--- fresh.sh
+++ fresh.sh
@@ -9,39 +9,49 @@ fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>$HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
-brew bundle
+brew bundle --file $DOTFILES/Brewfile
 
-# Set default MySQL root password and auth type.
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'a'; FLUSH PRIVILEGES;"
+# Set default MySQL root password and auth type
+mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
-pecl install imagick memcached redis swoole
+pecl install imagick redis swoole
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/valet
+/usr/local/bin/composer global require laravel/installer laravel/valet spatie/visit
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
 
-# Create a code directory
+# Create a Sites directory
 mkdir $HOME/code
 
 # Create code subdirectories
 mkdir $HOME/code/packages
 
 # Clone Github repositories
-./clone.sh
+$DOTFILES/clone.sh
 
-./link.sh
+# Link
+$DOTFILES/link.sh
 
-# Set macOS preferences
-# We will run this last because this will reload the shell
-source .macos
+# Symlink the Mackup config file to the home directory
+ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
+
+# Set macOS preferences - we will run this last because this will reload the shell
+source $DOTFILES/.macos
