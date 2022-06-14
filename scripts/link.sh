062f65a5b0c0c8730d2297b809476ce0ccb0976c
Keunbae Park <pkboom@users.noreply.github.com>
2022-06-14 09:49:25 -0400

    wip

--- scripts/link.sh
+++ scripts/link.sh
@@ -13,4 +13,5 @@ rm -rf "$HOME"/.gitignore_global
 ln -s "$HOME"/.dotfiles/.gitignore_global "$HOME"
 
 # Symlink the Mackup config file to the home directory
+rm -rf "$HOME"/.gitignore_global
 ln -s "$HOME"/.dotfiles/.mackup.cfg "$HOME"
