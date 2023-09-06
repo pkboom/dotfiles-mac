9954d6de0a1d3b558a07e59de120cdad2610107a
Keunbae Park <pkboom@users.noreply.github.com>
2023-09-06 14:27:55 -0400

    wip

--- test.sh
+++ test.sh
@@ -1,16 +1,3 @@
-#!/bin/zsh
+#!/bin/bash
 
-conda_line="#return 0 #anaconda"
-currentLine=$(grep $conda_line /Users/keunbae/.dotfiles/.zshrc)
-
-echo $currentLine
-
-if [ -z "$currentLine" ]; then
-  sed -i "" -e 's/^return 0 #anaconda/#return 0 #anaconda/g' /Users/keunbae/.dotfiles/.zshrc
-
-  echo "yes anaconda"
-else
-  sed -i "" -e 's/^#return 0 #anaconda/return 0 #anaconda/g' /Users/keunbae/.dotfiles/.zshrc
-
-  echo "no anaconda"
-fi
+sed -i "" -e 's/packagist.org/packagist_fake.org/' composer.json
