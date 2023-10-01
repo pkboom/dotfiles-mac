1927e24863851a28cd9a35f1ffbb4a25c8ce532f
Keunbae Park <pkboom@users.noreply.github.com>
2023-10-01 13:54:49 -0400

    wip

--- cron/emptyPhpFpmLog.sh
+++ cron/emptyPhpFpmLog.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # daily clean php-fpm logs
 # 0 0 * * * sh /Users/kevin/Dev/CRON/mac.sh
 LOGFILE=/opt/homebrew/var/log/php-fpm.log
@@ -6,8 +7,6 @@ LOGFILE=/opt/homebrew/var/log/php-fpm.log
 if test -f "$LOGFILE"; then
   echo "$LOGFILE exists."
   echo "" >"$LOGFILE"
-  echo "$LOGFILE contents:"
-  tail "$LOGFILE"
 else
   echo "$LOGFILE does not exist."
 fi
